7caeed53c2c50e8e012e6b0ce3dc307a0e41b71f
Benjamin Hepp <benjamin.hepp@posteo.de>
2017-07-05 18:02:55 -0700

    Added filtering option for depth map and some performance measurement code.
    * Image scale factor configurable
    * Better error handling

--- python/RLrecon/engine/engine.py
+++ python/RLrecon/engine/engine.py
@@ -1,64 +1,239 @@
+from __future__ import print_function
 import time
 import numpy as np
 import rospy
 from tf import transformations
 from RLrecon import math_utils
+from RLrecon.utils import Timer
 
 
 class BaseEngine(object):
 
-    @staticmethod
-    def _create_points_from_depth_image(depth_image, focal_length):
-        """Returns point cloud in camera frame. Assuming x-axis points forward, y-axis left and z-axis up."""
+    def __init__(self,
+                 max_depth_distance=np.finfo(np.float).max,
+                 max_depth_viewing_angle=math_utils.degrees_to_radians(90.)):
+        self._max_depth_distance = max_depth_distance
+        self._max_depth_viewing_dot_prod = np.cos(max_depth_viewing_angle)
+        self._ray_direction_image = None
+
+    def _compute_normal_image_dot_product(self, normal_image, other_vec):
+        dot_prod = np.tensordot(normal_image, other_vec, axes=1)
+        return dot_prod
+
+    def filter_normal_image(self, normal_image, ray_directions=None, inplace=True):
+        """Filter normal image based on maximum viewing angle"""
+        if ray_directions is None:
+            ray_directions = self.get_ray_direction_image_world(
+                normal_image.shape[1], normal_image.shape[0], self.get_focal_length())
+        # dot_prod = self._compute_normal_image_dot_product(normal_image, -view_direction)
+        dot_prod = np.sum(normal_image * (-ray_directions), axis=2)
+        if inplace:
+            filtered_normal_image = normal_image
+        else:
+            filtered_normal_image = np.array(normal_image)
+        filtered_normal_image[dot_prod < self._max_depth_viewing_dot_prod, :] = 0
+        return filtered_normal_image
+
+    def filter_depth_image(self, depth_image, normal_image, ray_directions=None, inplace=True):
+        """Filter depth image based on maximum viewing angle"""
+        if ray_directions is None:
+            ray_directions = self.get_ray_direction_image_world(
+                depth_image.shape[1], depth_image.shape[0], self.get_focal_length())
+        # dot_prod = self._compute_normal_image_dot_product(normal_image, -view_direction)
+        dot_prod = np.sum(normal_image * (-ray_directions), axis=2)
+        if inplace:
+            filtered_depth_image = depth_image
+        else:
+            filtered_depth_image = np.array(depth_image)
+        filtered_depth_image[depth_image > self._max_depth_distance] = -1.0
+        filtered_depth_image[dot_prod < self._max_depth_viewing_dot_prod] = -1.0
+        return filtered_depth_image
+
+    def _create_points_from_depth_image(self, depth_image, focal_length):
+        """Return point cloud in camera frame. Assuming x-axis points forward, y-axis left and z-axis up."""
+        # timer = Timer()
         height = depth_image.shape[0]
         width = depth_image.shape[1]
         x_c = np.float(width) / 2 - 1
         y_c = np.float(height) / 2 - 1
-        points = np.empty((depth_image.shape[0] * depth_image.shape[1], 3))
-        points[:, -1] = depth_image.flatten()
+        depth_image_flat = depth_image.flatten()
+        points = np.empty((depth_image_flat.shape[0], 3))
+        # t1 = timer.elapsed_seconds()
+        points[:, -1] = depth_image_flat
+        # t2 = timer.elapsed_seconds()
         columns, rows = np.meshgrid(np.linspace(0, width - 1, num=width), np.linspace(0, height - 1, num=height))
+        # t3 = timer.elapsed_seconds()
         points[:, 0] = points[:, -1] * (columns.flatten() - x_c) / focal_length
+        # t4 = timer.elapsed_seconds()
         points[:, 1] = points[:, -1] * (rows.flatten() - y_c) / focal_length
+        # t5 = timer.elapsed_seconds()
+        points = points[depth_image_flat > 0, :]
+        # t6 = timer.elapsed_seconds()
         # Transform points into camera coordinate system (x-axis forward, z-axis up, y-axis left)
         quat1 = transformations.quaternion_about_axis(-np.pi / 2, [1, 0, 0])
         quat2 = transformations.quaternion_about_axis(-np.pi / 2, [0, 0, 1])
         quat = transformations.quaternion_multiply(quat2, quat1)
+        # t7 = timer.elapsed_seconds()
         rot_mat = transformations.quaternion_matrix(quat)[:3, :3]
+        # t8 = timer.elapsed_seconds()
         points_transformed = points.dot(rot_mat.T)
+        # t9 = timer.elapsed_seconds()
+        # print("Timing of _create_points_from_depth_image:")
+        # print("  ", t1)
+        # print("  ", t2 - t1)
+        # print("  ", t3 - t2)
+        # print("  ", t4 - t3)
+        # print("  ", t5 - t4)
+        # print("  ", t6 - t5)
+        # print("  ", t7 - t6)
+        # print("  ", t8 - t7)
+        # print("  ", t9 - t8)
+        # print("Total: ", t9)
         return points_transformed
 
-    def get_depth_point_cloud_rpy(self, location, orientation_rpy):
+    def _convert_normal_rgb_image_to_normal_image(self, normal_rgb_image, inplace=True):
+        """Convert normal image in RGB encoding to normal image in vector encoding.
+        RGB encoding means 128 color value is 0, 0 color value is -1, 255 color value is +1.
+        """
+        if inplace:
+            assert(normal_rgb_image.dtype == np.float)
+            normal_image = normal_rgb_image
+        else:
+            normal_image = np.array(normal_rgb_image, dtype=np.float)
+        normal_image /= 0.5 * 255.
+        normal_image -= 1.0
+        # # For debugging. Show filtered depth image.
+        # assert(not np.any(normal_image < -1.))
+        # assert(not np.any(normal_image > 1.))
+        # import cv2
+        # normal_rgb_image_show = (normal_image + 1.0) * 0.5
+        # cv2.imshow('normal', normal_rgb_image_show)
+        # cv2.waitKey(10)
+        return normal_image
+
+    def get_view_direction_world(self):
+        """Return camera viewing direction in world frame.
+        Assuming x-axis points forward, y-axis left and z-axis up.
+        """
+        orientation_rpy = self.get_orientation_rpy()
+        quat = math_utils.convert_rpy_to_quat(orientation_rpy)
+        x_axis = [1, 0, 0]
+        view_direction = math_utils.rotate_vector_with_quaternion(quat, x_axis)
+        return view_direction
+
+    def get_ray_direction_image(self, width, height, focal_length):
+        """Return camera ray for given width, height and focal length.
+        Assuming x-axis points forward, y-axis left and z-axis up.
+        """
+        if self._ray_direction_image is not None:
+            return self._ray_direction_image
+        x_c = np.float(width) / 2 - 1
+        y_c = np.float(height) / 2 - 1
+        ray_directions = np.empty((height, width, 3))
+        columns, rows = np.meshgrid(np.linspace(0, width - 1, num=width), np.linspace(0, height - 1, num=height))
+        ray_directions[:, :, 0] = 1.
+        # Sign swap because y is pointing to the left
+        ray_directions[:, :, 1] = -(columns - x_c) / focal_length
+        # Sign swap because of image y coordinate starting with 0 at the top
+        ray_directions[:, :, 2] = -(rows - y_c) / focal_length
+        # Normalize ray directions
+        ray_directions /= np.sum(ray_directions ** 2, axis=2)[:, :, np.newaxis]
+        # # For debugging. Show filtered depth image.
+        # print("center ray:", ray_directions[height/2, width/2, :])
+        # print("top ray:", ray_directions[0, width/2, :])
+        # print("bottom ray:", ray_directions[height-1, width/2, :])
+        # print("left ray:", ray_directions[height/2, 0, :])
+        # print("right ray:", ray_directions[height/2, width-1, :])
+        # import cv2
+        # ray_directions_show = (-ray_directions + 1.0) * 0.5
+        # cv2.imshow('ray_directions', ray_directions_show)
+        # cv2.waitKey(10)
+        return ray_directions
+
+    def get_ray_direction_image_world(self, width, height, focal_length, orientation_rpy=None):
+        """Return camera ray for given width, height and focal length in world frame.
+        Assuming x-axis points forward, y-axis left and z-axis up.
+        """
+        ray_directions = self.get_ray_direction_image(width, height, focal_length)
+        # Transform ray directions into world-frame
+        if orientation_rpy is None:
+            orientation_rpy = self.get_orientation_rpy()
+        orientation_quat = math_utils.convert_rpy_to_quat(orientation_rpy)
+        rot_mat = transformations.quaternion_matrix(orientation_quat)[:3, :3]
+        ray_directions_world = ray_directions.dot(rot_mat.T)
+        return ray_directions_world
+
+    def get_depth_point_cloud_rpy(self, location, orientation_rpy, filter=True):
+        """Return point cloud (from depth image) in local coordinate frame"""
+        # timer = Timer()
         self.set_location(location)
         roll, pitch, yaw = orientation_rpy
         self.set_orientation_rpy(roll, pitch, yaw)
         focal_length = self.get_focal_length()
+        # t1 = timer.elapsed_seconds()
         # TODO: Seems to be unnecessary now.
         # Make sure that the new frame is rendered.
         # This should be fixed in the UnrealCV plugin.
-        quat = math_utils.convert_rpy_to_quat(orientation_rpy)
-        max_delay_time = 2.0
-        t0 = time.time()
-        while time.time() - t0 < max_delay_time:
-            if math_utils.is_vector_equal(location, self.get_location(), tolerance=1e-4) \
-                    and math_utils.is_equal_quaternion(quat, self.get_orientation_quat(), tolerance=1e-4):
-                break
-            time.sleep(0.01)
+        # TODO: Seems to be unnecessary now.
+        # quat = math_utils.convert_rpy_to_quat(orientation_rpy)
+        # max_delay_time = 2.0
+        # t0 = time.time()
+        # t2 = timer.elapsed_seconds()
+        # while time.time() - t0 < max_delay_time:
+        #     if math_utils.is_vector_equal(location, self.get_location(), tolerance=1e-4) \
+        #             and math_utils.is_equal_quaternion(quat, self.get_orientation_quat(), tolerance=1e-4):
+        #         break
+        #     time.sleep(0.01)
+        # t3 = timer.elapsed_seconds()
         # TODO: Seems to be unnecessary now.
         # After the pose was successfully updated, wait a little bit longer.
         # time.sleep(0.1)
         # t1 = time.time()
         # print("delay={}".format(t1 - t0))
         depth_image = self.get_depth_image()
-        rospy.logdebug("min depth={}, max depth={}".format(
+        # t4 = timer.elapsed_seconds()
+        rospy.logdebug("Depth image: min={}, max={}".format(
             np.min(depth_image.flatten()), np.max(depth_image.flatten())))
-
+        if filter:
+            normal_image = self.get_normal_image()
+            # t5 = timer.elapsed_seconds()
+            ray_directions = self.get_ray_direction_image_world(
+                depth_image.shape[1], depth_image.shape[0],
+                self.get_focal_length(), orientation_rpy)
+            # t6 = timer.elapsed_seconds()
+            self.filter_depth_image(depth_image, normal_image, ray_directions, inplace=True)
+            # t7 = timer.elapsed_seconds()
+            rospy.logdebug("Filtered depth image: min={}, max={}".format(
+                np.min(depth_image.flatten()), np.max(depth_image.flatten())))
+            # # For debugging. Show filtered depth image.
+            # import cv2
+            # depth_image_show = depth_image / 20.
+            # depth_image_show[depth_image_show > 1] = 1
+            # depth_image_show[depth_image_show < 0] = 0
+            # cv2.imshow('depth', depth_image_show)
+            # cv2.waitKey(10)
         # Create pointcloud message
-        rospy.logdebug("Creating point cloud message")
+        rospy.loginfo("Creating point cloud message")
         points = self._create_points_from_depth_image(depth_image, focal_length)
+        # t8 = timer.elapsed_seconds()
+        # print("Timing of get_depth_point_cloud_rpy():")
+        # print("  ", t1)
+        # print("  ", t2 - t1)
+        # print("  ", t3 - t2)
+        # print("  ", t4 - t3)
+        # if filter:
+        #     print("  ", t5 - t4)
+        #     print("  ", t6 - t5)
+        #     print("  ", t7 - t6)
+        #     print("  ", t8 - t7)
+        # else:
+        #     print("  ", t8 - t4)
+        # print("Total: ", t8)
         return points
 
-    def get_depth_point_cloud_world_rpy(self, location, orientation_rpy):
-        points = self.get_depth_point_cloud_rpy(location, orientation_rpy)
+    def get_depth_point_cloud_world_rpy(self, location, orientation_rpy, filter=True):
+        """Return point cloud (from depth image) in world coordinate frame"""
+        points = self.get_depth_point_cloud_rpy(location, orientation_rpy, filter=filter)
         world_quat = math_utils.convert_rpy_to_quat(orientation_rpy)
         world_trans = location
         world_rot_mat = transformations.quaternion_matrix(world_quat)[:3, :3]
--- python/RLrecon/engine/unreal_cv_wrapper.py
+++ python/RLrecon/engine/unreal_cv_wrapper.py
@@ -11,10 +11,14 @@ from RLrecon import math_utils
 
 class UnrealCVWrapper(BaseEngine):
 
-    class Exception(BaseException):
+    class Exception(RuntimeError):
         pass
 
-    def __init__(self, address=None, port=None, image_scale_factor=0.5):
+    def __init__(self, address=None, port=None,
+                 image_scale_factor=0.5,
+                 max_depth_distance=np.finfo(np.float).max,
+                 max_depth_viewing_angle=math_utils.degrees_to_radians(90.)):
+        super(UnrealCVWrapper, self).__init__(max_depth_distance, max_depth_viewing_angle)
         self._width = None
         self._height = None
         self._image_scale_factor = image_scale_factor
@@ -25,7 +29,7 @@ class UnrealCVWrapper(BaseEngine):
         self._cv_client = Client((address, port))
         self._cv_client.connect()
         if not self._cv_client.isconnected():
-            raise(Exception("Unable to connect to UnrealCV"))
+            raise(self.Exception("Unable to connect to UnrealCV"))
 
     def _scale_image(self, image, interpolation_mode=cv2.INTER_CUBIC):
         """Scale an image to the desired size"""
@@ -74,6 +78,8 @@ class UnrealCVWrapper(BaseEngine):
     def get_rgb_image(self):
         """Return the current RGB image"""
         img_str = self._cv_client.request('vget /camera/0/lit png')
+        if img_str is None:
+            raise self.Exception("UnrealCV request failed")
         img = np.fromstring(img_str, np.uint8)
         rgb_image = cv2.imdecode(img, cv2.IMREAD_COLOR)
         rgb_image = self._scale_image(rgb_image)
@@ -82,30 +88,61 @@ class UnrealCVWrapper(BaseEngine):
     def get_rgb_image_by_file(self):
         """Return the current RGB image (transport via filesystem)"""
         filename = self._cv_client.request('vget /camera/0/lit lit.png')
+        if filename is None:
+            raise self.Exception("UnrealCV request failed")
         rgb_image = cv2.imread(filename)
         rgb_image = self._scale_image(rgb_image)
         os.remove(filename)
         return rgb_image
 
-    def get_normal_image(self):
-        """Return the current normal image"""
+    def get_normal_rgb_image(self):
+        """Return the current normal image in RGB encoding (i.e. 128 is 0, 0 is -1, 255 is +1)"""
         img_str = self._cv_client.request('vget /camera/0/normal png')
+        if img_str is None:
+            raise self.Exception("UnrealCV request failed")
         img = np.fromstring(img_str, np.uint8)
         normal_image = cv2.imdecode(img, cv2.IMREAD_COLOR)
         normal_image = self._scale_image_with_nearest_interpolation(normal_image)
         return normal_image
 
-    def get_normal_image_by_file(self):
-        """Return the current normal image (transport via filesystem)"""
+    def get_normal_rgb_image_by_file(self):
+        """Return the current normal image in RGB encoding (i.e. 128 is 0, 0 is -1, 255 is +1)
+        (transport via filesystem)
+        """
         filename = self._cv_client.request('vget /camera/0/normal normal.png')
+        if filename is None:
+            raise self.Exception("UnrealCV request failed")
         normal_image = cv2.imread(filename)
         normal_image = self._scale_image_with_nearest_interpolation(normal_image)
         os.remove(filename)
         return normal_image
 
+    def get_normal_image(self):
+        """Return the current normal image in vector representation"""
+        normal_rgb_image = self.get_normal_rgb_image()
+        # Image is in BGR ordering, so [z, y, x]. Convert to RGB.
+        normal_rgb_image = normal_rgb_image[:, :, ::-1]
+        normal_image = self._convert_normal_rgb_image_to_normal_image(normal_rgb_image, inplace=False)
+        # Convert left-handed Unreal system to right-handed system
+        normal_image[:, :, 1] = -normal_image[:, :, 1]
+        self.filter_normal_image(normal_image)
+        return normal_image
+
+    def get_normal_image_by_file(self):
+        """Return the current normal image in vector representation (transport via filesystem)"""
+        normal_rgb_image = self.get_normal_rgb_image_by_file()
+        # Image is in BGR ordering, so [z, y, x]. Convert to RGB.
+        normal_rgb_image = normal_rgb_image[:, :, ::-1]
+        normal_image = self._convert_normal_rgb_image_to_normal_image(normal_rgb_image, inplace=False)
+        # Convert left-handed Unreal system to right-handed system
+        normal_image[:, :, 1] = -normal_image[:, :, 1]
+        return normal_image
+
     def get_ray_distance_image(self):
         """Return the current ray-distance image"""
         img_str = self._cv_client.request('vget /camera/0/depth npy')
+        if img_str is None:
+            raise self.Exception("UnrealCV request failed")
         img_str_io = StringIO(img_str)
         ray_distance_image = np.load(img_str_io)
         ray_distance_image = self._scale_image_with_nearest_interpolation(ray_distance_image)
@@ -114,6 +151,8 @@ class UnrealCVWrapper(BaseEngine):
     def get_ray_distance_image_by_file(self):
         """Return the current ray-distance image (transport via filesystem)"""
         filename = self._cv_client.request('vget /camera/0/depth depth.exr')
+        if filename is None:
+            raise self.Exception("UnrealCV request failed")
         ray_distance_image = cv2.imread(filename, cv2.IMREAD_ANYDEPTH)
         ray_distance_image = self._scale_image_with_nearest_interpolation(ray_distance_image)
         os.remove(filename)
@@ -136,6 +175,8 @@ class UnrealCVWrapper(BaseEngine):
     def get_location(self):
         """Return the current location in meters as [x, y, z]"""
         location_str = self._cv_client.request('vget /camera/0/location')
+        if location_str is None:
+            raise self.Exception("UnrealCV request failed")
         location_unreal = np.array([float(v) for v in location_str.split()])
         # Convert location from Unreal (cm) to meters
         location_unreal *= 0.01
@@ -146,6 +187,8 @@ class UnrealCVWrapper(BaseEngine):
     def get_orientation_rpy(self):
         """Return the current orientation in radians as [roll, pitch, yaw]"""
         orientation_str = self._cv_client.request('vget /camera/0/rotation')
+        if orientation_str is None:
+            raise self.Exception("UnrealCV request failed")
         pitch, yaw, roll = [float(v) * np.pi / 180. for v in orientation_str.split()]
         euler_rpy = np.array([roll, pitch, yaw])
         roll, pitch, yaw = math_utils.convert_rpy_from_left_to_right_handed(euler_rpy)
@@ -186,7 +229,9 @@ class UnrealCVWrapper(BaseEngine):
         request_str = 'vset /camera/0/location {:f} {:f} {:f}'.format(
             location_unreal[0], location_unreal[1], location_unreal[2])
         # print("Sending location request: {}".format(request_str))
-        self._cv_client.request(request_str)
+        response = self._cv_client.request(request_str)
+        if response != "ok":
+            raise self.Exception("UnrealCV request failed: {}".format(response))
 
     def set_orientation_rpy(self, roll, pitch, yaw):
         """Set new orientation in radians"""
@@ -195,7 +240,9 @@ class UnrealCVWrapper(BaseEngine):
         request_str = 'vset /camera/0/rotation {:f} {:f} {:f}'.format(
             pitch * 180 / np.pi, yaw * 180 / np.pi, roll * 180 / np.pi)
         # print("Sending orientation request: {}".format(request_str))
-        self._cv_client.request(request_str)
+        response = self._cv_client.request(request_str)
+        if response != "ok":
+            raise self.Exception("UnrealCV request failed: {}".format(response))
 
     def set_orientation_quat(self, quat):
         """Set new orientation quaterion quat = [w, x, y, z]"""
